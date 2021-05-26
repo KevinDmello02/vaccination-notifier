a6a1bac16b71a9e076965ffdead1023b01b2a2f6
kevindmello02 <kevindmello05@gmail.com>
2021-05-26 21:38:33 +0530

    updated cron job and array structure

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const handlebars = require('handlebars');
+const fs = require('fs');
+const path = require('path')
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
@@ -5,19 +9,19 @@ const request = require('request');
 const cron = require('node-cron');
 const moment = require('moment');
 const nodemailer = require("nodemailer");
+
+var ejs = require("ejs");
+
 require('dotenv').config()
 
 app.use(express.json());
 
-// app.use(express.urlencoded());
-
-
-
-cron.schedule('* * * * *', function() {
+cron.schedule('* 2 * * *', function() {
     console.log('running a task every minute');
     sendRequests();
 });
-// sendMail(0)
+
+sendRequests();
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
@@ -28,68 +32,73 @@ app.listen(port, () => {
 
 function sendRequests() {
     let pincodes = ['401301', '401303', '401304', '421303', '400103'];
-    let district = ['395', '395']
+    let district = ['395', '394']
+    let objArr = [];
     let date = new Date;
     date = moment(date).format('DD-MM-YYYY');
-    // for (let pin = 0; pin < pincodes.length; pin++) {
-    //     request(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincodes[pin]}&date=26-05-2021}`, { json: true }, (err, res, body) => {
-    //         if (err) { return console.log(err); }  
-    //         let data = body;      
-    //         data = data.centers;
-    //         for (let i = 0; i < data.length; i++) {
-    //             console.log(data[i])
-    //             if (data[i].sessions[0].available_capacity_dose1 !=0 ) {
-    //                 sendMail(data[i]);
-    //             } else {
-    //                 dontSendMail();
-    //             }
-    //         }
-    //     });
-    // } 
     for (let dis = 0; dis < district.length; dis++) {
         request(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district[dis]}&date=27-05-2021`, { json: true }, (err, res, body) => {
             if (err) { return console.log(err); }  
             let data = body;      
-            // data = data.centers;
             for (let i = 0; i < data.sessions.length; i++) {
                 if (data.sessions[i].available_capacity_dose1 != 0 && data.sessions[i].fee_type == 'Free' ) {
-                    sendMail(data.sessions[i]);
+                    let obj = {
+                        name: data.sessions[i].name,
+                        address: data.sessions[i].address,
+                        pincode: data.sessions[i].pincode,
+                        available: data.sessions[i].available_capacity_dose1 
+                    }
+                    objArr.push(obj);
+                    obj = {};
                 } else {
                     dontSendMail();
                 }
             }
+            sendMail(objArr);
         });
     } 
     return;
 }
 
 function sendMail(data) {
-    console.log(data)
+    const filePath = path.join(__dirname, './template/mail.ejs');
+    const source = fs.readFileSync(filePath, 'utf-8').toString();
+    const template = handlebars.compile(source);
+    const htmlToSend = template(data);
+
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             type: 'OAuth2',
             user: process.env.MAIL_USERNAME,
-            // pass: process.env.MAIL_PASSWORD,
             clientId: process.env.OAUTH_CLIENTID,
             clientSecret: process.env.OAUTH_CLIENT_SECRET,
             refreshToken: process.env.OAUTH_REFRESH_TOKEN
         }
     });
-    let mailOptions = {
-        from: 'kevindmello05@gmail.com',
-        to: 'kevindmello05@gmail.com, unitedkumbharwada@gmail.com',
-        subject: 'Vaccination Status',
-        // text: `${data.name} address: ${data.address} pincode: ${data.pincode} has ${data.available_capacity_dose1} slots Available!. NOTE: This is an auto generated mail.`,
-        html: `<b>${data.name}<br>address: ${data.address} <br>pincode: ${data.pincode} <br>has <span style="color: green">${data.available_capacity_dose1}</span> slots Available!.</b> <br><span style="color:red;">NOTE: This is an auto generated mail.</span>`
-    };
-    transporter.sendMail(mailOptions, function(err, data) {
+
+    ejs.renderFile("./template/mail.ejs", { data }, function(err, data) {
         if (err) {
-          console.log("Error " + err);
+            console.log(err);
         } else {
-          console.log("Email sent successfully");
+            let mailOptions = {
+                from: 'kevindmello05@gmail.com',
+                to: 'kevindmello05@gmail.com, unitedkumbharwada@gmail.com',
+                subject: 'Vaccination Status',
+                html: data
+            };
+            transporter.sendMail(mailOptions, function(err, data) {
+                if (err) {
+                  console.log("Error " + err);
+                } else {
+                  console.log("Email sent successfully");
+                }
+            });
         }
-    });
+    })
 }
+
+
+
 function dontSendMail() {
 }
